4297049f2ebd3ae3dcdb75ff4ff6771a0284189e
NortKron <aksogol@gmail.com>
2023-10-24 16:23:22 +0600

    Updated settings of the project

--- src/main/java/com/astrology/config/AstrologyApplication.java
+++ src/main/java/com/astrology/config/AstrologyApplication.java
@@ -1,15 +1,25 @@
 package com.astrology.config;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
+@ComponentScan("com.astrology")
 public class AstrologyApplication
 {
+	//private static Logger LOG = LoggerFactory.getLogger(AstrologyApplication.class);
+
 	public static void main(String[] args)
 	{
 		System.out.println("----------> Start!! <----------");
 		SpringApplication.run(AstrologyApplication.class, args);
+		
+		//ApplicationContext ctx = SpringApplication.run(AstrologyApplication.class, args);
+		//DispatcherServlet dispatcherServlet = (DispatcherServlet) ctx.getBean("dispatcherServlet");
+		//dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
 	}
-
 }
